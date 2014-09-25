8f8966b307e44efb6a03c6b336a5b414689381fe
csquared <christopher.continanza@gmail.com>
2014-09-24 20:59:31 -0700

    refactor

---
+++ pipe.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"sync"
+)
+
+func pipeToHttp(reader io.Reader, url string, bufSize int) {
+	buffer := bufio.NewReader(os.Stdin)
+	lineBuffer := make([]string, bufSize)
+
+	i := 0
+	var wg sync.WaitGroup
+	for {
+		line, err := buffer.ReadString('\n')
+		// ship whatever is in the buffer at EOF
+		if err == io.EOF {
+			_ = shipLinesToUrl(lineBuffer[0:i], url)
+			break
+		}
+		if err != nil {
+			panic("Error reading data from buffer")
+		}
+		// put lines in the buffer; ship them
+		// when the buffer is full
+		if line != "" {
+			lineBuffer[i] = line
+			i++
+			if i == cap(lineBuffer) {
+				wg.Add(1)
+				go func(buf []string, _url string) {
+					defer wg.Done()
+					shipLinesToUrl(buf, _url)
+				}(lineBuffer, url)
+				i = 0
+			}
+		}
+	}
+	wg.Wait()
+}
